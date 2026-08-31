5b96d4ebe49f094a2cfa0d8fa1583087061d7f5c
Sadman Habib <sadman.habib47@myhunter.cuny.edu>
2026-08-31 11:43:00 -0400

    Format Quodex cask

--- Casks/quodex.rb
+++ Casks/quodex.rb
@@ -2,18 +2,21 @@ cask "quodex" do
   version "0.2.0"
 
   on_arm do
-    url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-arm64.dmg"
     sha256 "4279d494c9285de95c800cd4755b1c33213c085e94dcc0303c64bcb73c8a950d"
-  end
 
+    url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-arm64.dmg"
+  end
   on_intel do
-    url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-x86_64.dmg"
     sha256 "c6922662399ec0f9e0552b09ea9717f15e4dde01f9f1d495c1104d4a8d658e9c"
+
+    url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-x86_64.dmg"
   end
 
   name "Quodex"
-  desc "Native macOS menu-bar usage tracker for multiple ChatGPT accounts"
+  desc "Native menu-bar usage tracker for multiple ChatGPT accounts"
   homepage "https://github.com/hxbib/Quodex"
+
+  depends_on macos: :sonoma
 
   app "Quodex.app"
 end
