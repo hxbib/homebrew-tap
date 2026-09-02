1d5ceeb8ae87d005e85979a42b954cee4420dbf0
Sadman Habib <sadman.habib47@myhunter.cuny.edu>
2026-09-02 12:32:21 -0400

    Update Quodex 0.2.0 checksums

--- Casks/quodex.rb
+++ Casks/quodex.rb
@@ -2,12 +2,12 @@ cask "quodex" do
   version "0.2.0"
 
   on_arm do
-    sha256 "5436a6a980b54689f41cc79bf4011110f092638ffe15a637191399f12d5adca7"
+    sha256 "177f236e992fb43393244f6ad81e96e5673a2abb553e7b7e75e4788f8660b398"
 
     url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "a068c52171d69e0ec25f838118e6ed44ff47ff622425bc3873068c28e753ca8b"
+    sha256 "cd6c5eecb5cef0561d052b3d1a67ced3c478e8be7d46e2dad70916146b98cb6a"
 
     url "https://github.com/hxbib/Quodex/releases/download/v#{version}/Quodex-#{version}-x86_64.dmg"
   end
